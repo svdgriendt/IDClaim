0769dec465af1b44e2da6cdf7fd0461b4c186db7
Stefan van de Griendt <s.van.de.griendt@outlook.com>
2017-08-19 22:56:37 +0200

    Made tables memory optimized
    Improves performance and supports compiled atomic stored procedures

--- IDClaim.Database/IDClaim.sql
+++ IDClaim.Database/IDClaim.sql
@@ -8,19 +8,5 @@
   [DefaultClaimSize] INT NOT NULL,
 
   CONSTRAINT [pkIDClaim] PRIMARY KEY NONCLUSTERED ([ID]),
-  CONSTRAINT [ucIDClaim] UNIQUE CLUSTERED ([Database], [Schema], [Table])
-);
-GO
-
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Primary ID',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'ID'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the database for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Database'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the schema for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Schema'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the table for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Table'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'First available not claimed ID',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'StartFreeRange'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Default size of the range of IDs to claim at once',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'DefaultClaimSize'
-GO
+  CONSTRAINT [ucIDClaim] UNIQUE NONCLUSTERED HASH ([Database], [Schema], [Table]) WITH (BUCKET_COUNT = 128)
+) WITH (MEMORY_OPTIMIZED = ON, DURABILITY = SCHEMA_AND_DATA);
--- IDClaim.Database/IDClaimHistory.sql
+++ IDClaim.Database/IDClaimHistory.sql
@@ -6,18 +6,6 @@
   [RangeEnd] BIGINT NOT NULL,
   [Requestor] VARCHAR(64) NOT NULL,
 
-  CONSTRAINT [pkIDClaimHistory] PRIMARY KEY CLUSTERED ([ID], [IssueDate] DESC),
+  CONSTRAINT [pkIDClaimHistory] PRIMARY KEY NONCLUSTERED ([ID], [IssueDate] DESC),
   CONSTRAINT [fkIDClaimHistory_IDClaim] FOREIGN KEY ([ID]) REFERENCES [dbo].[IDClaim]([ID])
-);
-GO
-
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Referenced ID', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'ID'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Date of issue of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'IssueDate'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Inclusive starting ID of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'RangeStart'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Inclusive ending ID of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'RangeEnd'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Name of the requesting module', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'Requestor'
-GO
+) WITH (MEMORY_OPTIMIZED = ON, DURABILITY = SCHEMA_AND_DATA);
