6d2093e441393e376a7bbbab63c1cfb9f3ccaf2d
Stefan van de Griendt <s.van.de.griendt@outlook.com>
2017-08-19 22:57:32 +0200

    Added stored procedures and type
    Type is required because no inline tables can be created in compiled stored procedures, which is a requirement to make them atomic

---
+++ IDClaim.Database/CreateClaim.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[CreateClaim]
+  @Database SYSNAME NOT NULL,
+  @Schema SYSNAME NOT NULL,
+  @Table SYSNAME NOT NULL,
+  @StartFreeRange BIGINT NOT NULL,
+  @DefaultClaimSize INT NOT NULL
+WITH NATIVE_COMPILATION, SCHEMABINDING
+AS
+BEGIN ATOMIC WITH (TRANSACTION ISOLATION LEVEL = SNAPSHOT, LANGUAGE = 'us_english')
+  DECLARE @Exists BIT = (SELECT TOP 1 1
+                         FROM [dbo].[IDClaim]
+                         WHERE [Database] = @Database AND [Schema] = @Schema AND [Table] = @Table)
+  IF @Exists IS NULL
+    INSERT INTO [dbo].[IDClaim] VALUES (@Database, @Schema, @Table, @StartFreeRange, @DefaultClaimSize);
+END
