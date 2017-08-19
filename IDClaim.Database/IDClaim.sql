fc82b268c583d2e19d4e430faf071219bbe1d231
Stefan van de Griendt <s.van.de.griendt@outlook.com>
2017-08-19 21:13:46 +0200

    Added tables

---
+++ IDClaim.Database/IDClaim.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[IDClaim]
+(
+  [ID] BIGINT NOT NULL IDENTITY(1,1),
+  [Database] SYSNAME NOT NULL,
+  [Schema] SYSNAME NOT NULL,
+  [Table] SYSNAME NOT NULL,
+  [StartFreeRange] BIGINT NOT NULL,
+  [DefaultClaimSize] INT NOT NULL,
+
+  CONSTRAINT [pkIDClaim] PRIMARY KEY NONCLUSTERED ([ID]),
+  CONSTRAINT [ucIDClaim] UNIQUE CLUSTERED ([Database], [Schema], [Table])
+);
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Primary ID',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'ID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the database for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Database'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the schema for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Schema'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Name of the table for the claims',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'Table'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'First available not claimed ID',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'StartFreeRange'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',  @value = N'Default size of the range of IDs to claim at once',  @level0type = N'SCHEMA',  @level0name = N'dbo',  @level1type = N'TABLE',  @level1name = N'IDClaim',  @level2type = N'COLUMN',  @level2name = N'DefaultClaimSize'
+GO
---
+++ IDClaim.Database/IDClaimHistory.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[IDClaimHistory]
+(
+  [ID] BIGINT NOT NULL,
+  [IssueDate] DATETIME2(2) NOT NULL,
+  [RangeStart] BIGINT NOT NULL,
+  [RangeEnd] BIGINT NOT NULL,
+  [Requestor] VARCHAR(64) NOT NULL,
+
+  CONSTRAINT [pkIDClaimHistory] PRIMARY KEY CLUSTERED ([ID], [IssueDate] DESC),
+  CONSTRAINT [fkIDClaimHistory_IDClaim] FOREIGN KEY ([ID]) REFERENCES [dbo].[IDClaim]([ID])
+);
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Referenced ID', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'ID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Date of issue of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'IssueDate'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Inclusive starting ID of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'RangeStart'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Inclusive ending ID of the range', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'RangeEnd'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description', @value = N'Name of the requesting module', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'IDClaimHistory', @level2type = N'COLUMN', @level2name = N'Requestor'
+GO
